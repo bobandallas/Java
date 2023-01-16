674cd8624decf44894bec938d985fa62e4fb8c7e
bobandallas <bobandallas@gmail.com>
2023-01-16 01:34:34 -0600

    up

---
+++ src/main/java/com/example/java26/week3/rest/homework/utility/DateUtility.java
@@ -0,0 +1,12 @@
+package com.example.java26.week3.rest.homework.utility;
+
+import java.util.Date;
+
+public class DateUtility {
+
+    private DateUtility() {}
+
+    public static Date getCurrentDate() {
+        return new Date();
+    }
+}
